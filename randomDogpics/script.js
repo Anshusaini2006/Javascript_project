39e310c17c4ee8b34f391c871dfc57951b418dde
Anshusaini2006 <dsanshu14@gmail.com>
2025-01-28 13:10:53 +0530

    randomDogpics completed

---
+++ randomDogpics/script.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById("fetchDogbtn")
+const image = document.getElementById("dogImage")
+
+function fetchDog() {
+    return new Promise((resolve,reject)=>{
+     fetch("https://dog.ceo/api/breeds/image/random")
+     .then((response)=>{
+        console.log("Response received:",response);
+      return response.json();
+     })
+.then((data)=>{
+    resolve(data);
+}).catch((error)=>{
+    reject(error);
+});
+    });
+};
+
+
+btn.addEventListener("click",()=>{
+    console.log("Fetchng a random dog picture...");
+    fetchDog()
+    .then((data)=>{
+  console.log("Data fetched from API:",data);
+  image.src = data.message;
+    }).catch((error)=>{
+    image.src= error
+    })
+})
